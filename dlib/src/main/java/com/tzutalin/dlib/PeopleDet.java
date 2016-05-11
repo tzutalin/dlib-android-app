4094fda7f21361b25e90026c94ebcb1a798884ec
flyingzhao <flyingzhao@users.noreply.github.com>
2016-05-11 16:23:45 +0800

    import bitmap package
    import bitmap package

--- dlib/src/main/java/com/tzutalin/dlib/PeopleDet.java
+++ dlib/src/main/java/com/tzutalin/dlib/PeopleDet.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 import android.util.Log;
+import android.graphics.Bitmap;
 
 import java.io.File;
 import java.util.ArrayList;

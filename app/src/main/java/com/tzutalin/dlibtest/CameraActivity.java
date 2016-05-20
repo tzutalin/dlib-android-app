40f07ba071761a998548043528521c3a4150c981
tzutalin <tzu.ta.lin@gmail.com>
2016-05-20 18:18:38 +0800

    Upload camera code

---
+++ app/src/main/java/com/tzutalin/dlibtest/CameraActivity.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 Tzutalin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tzutalin.dlibtest;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.WindowManager;
+/**
+ * Created by darrenl on 2016/5/20.
+ */
+public class CameraActivity extends Activity {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, CameraConnectionFragment.newInstance())
+                    .commit();
+        }
+    }
+}
